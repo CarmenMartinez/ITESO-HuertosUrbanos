de6977410ac6d9136bd20e3e474e6c2c802b1e2d
Carmen Martinez <carmen.mtzn@gmail.com>
2017-10-26 10:49:33 -0500

    create account activity

---
+++ WeHarvest/app/src/main/java/com/weharvest/ActivityCreateAccount.java
@@ -0,0 +1,13 @@
+package com.weharvest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityCreateAccount extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+    }
+}
